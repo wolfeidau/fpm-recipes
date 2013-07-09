3a6ec624b69015ad2d3966955ff485fc25f7ee74
Mark Wolfe <mark@wolfe.id.au>
2013-07-09 21:16:24 +1000

    More resources for my mosquitto recipe.

--- mosquitto/recipe.rb
+++ mosquitto/recipe.rb
@@ -3,18 +3,28 @@ class Mosquitto < FPM::Cookery::Recipe
 
   name     'mosquitto'
   version  '1.1.3'
+  revision 1
   homepage 'http://mosquitto.org'
   source   "http://mosquitto.org/files/source/mosquitto-#{version}.tar.gz"
   sha1     '38faf05f696b6a8183c6f5e06fdba78ffb632316'
-
+  
+  section   'database'
   conflicts 'mosquitto'
   replaces  'mosquitto'
+  
+  config_files '/etc/mosquitto/mosquitto.conf'
+  post_install 'postinst'
 
   def build
     make
   end
 
   def install
+    var('log/mosquitto').mkpath
+
+    etc('default').install_p workdir('mosquitto.default'), 'mosquitto'
+    etc('init').install_p builddir('mosquitto.upstart'), 'mosquitto.conf'
+
     make :install, 'DESTDIR' => destdir
   end
 end
