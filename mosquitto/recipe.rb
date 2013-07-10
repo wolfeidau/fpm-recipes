5202a89753b5012e18d3805e1570d70cf8fe4f27
Mark Wolfe <mark@wolfe.id.au>
2013-07-10 13:57:28 +1000

    Added configuration and startup script.

--- mosquitto/recipe.rb
+++ mosquitto/recipe.rb
@@ -1,5 +1,5 @@
 class Mosquitto < FPM::Cookery::Recipe
-  description 'terminal multiplexer'
+  description 'Mosquitto is an open source (BSD licensed) message broker that implements the MQ Telemetry Transport protocol version 3.1.'
 
   name     'mosquitto'
   version  '1.1.3'
@@ -8,7 +8,7 @@ class Mosquitto < FPM::Cookery::Recipe
   source   "http://mosquitto.org/files/source/mosquitto-#{version}.tar.gz"
   sha1     '38faf05f696b6a8183c6f5e06fdba78ffb632316'
   
-  section   'database'
+  section   'network'
   conflicts 'mosquitto'
   replaces  'mosquitto'
   
@@ -24,6 +24,7 @@ class Mosquitto < FPM::Cookery::Recipe
 
     etc('default').install_p workdir('mosquitto.default'), 'mosquitto'
     etc('init').install_p builddir('mosquitto.upstart'), 'mosquitto.conf'
+    etc('mosquitto').install_p workdir('mosquitto.conf'), 'mosquitto.conf'
 
     make :install, 'DESTDIR' => destdir
   end
